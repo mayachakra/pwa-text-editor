d43de08af6507b5a08fae5e97135bbac63eded96
mayachakra <mayajchakravarty@gmail.com>
2024-05-02 15:23:54 -0400

    added changes to database so header appears and image

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -11,19 +11,63 @@ module.exports = () => {
     mode: 'development',
     entry: {
       main: './src/js/index.js',
-      install: './src/js/install.js'
+      install: './src/js/install.js',
+      editor: './src/js/editor.js',
+      header: './src/js/header.js'
     },
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Webpack Plugin',
+      }),
+      // Injects our custom service worker
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+        //swDest: 'service-worker.js',
+
+      }),
+      //add WebpackPwaManifest({}),
+      new WebpackPwaManifest({
+        // TODO: Create a manifest.json:
+        name: 'My Text-Editor App',
+        short_name: 'Text-Editor',
+        description: 'An app to use your Text-Editor!',
+        background_color: '#ffffff',
+        publicPath: './',
+        fingerprints: false,
+        icons: [
+      {
+        src: path.resolve('src/images/logo.png'),
+        sizes: [96, 128, 192, 256, 384, 512], // multiple sizes
+        destination: path.join('assets', 'icons'),
+      },
+      ],
+      start_url: './',
+      display: 'standalone',
+      }),
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        }
       ],
     },
   };
